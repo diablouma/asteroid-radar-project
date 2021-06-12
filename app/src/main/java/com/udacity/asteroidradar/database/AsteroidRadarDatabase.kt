496c2ed1c6e323380b28b1de20b016737d898f8a
Nelson Jumbo <nelsonjumbo.tw@packlink.com>
2021-06-12 15:15:42 +0200

    configured room database

---
+++ app/src/main/java/com/udacity/asteroidradar/database/AsteroidRadarDatabase.kt
@@ -0,0 +1,33 @@
+package com.udacity.asteroidradar.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [], version = 1, exportSchema = false)
+abstract class AsteroidRadarDatabase : RoomDatabase() {
+    companion object {
+        @Volatile
+        private var INSTANCE: AsteroidRadarDatabase? = null
+
+        fun getInstance(context: Context): AsteroidRadarDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        AsteroidRadarDatabase::class.java,
+                        "asteroid_radar_database"
+                    ).build()
+                }
+
+                INSTANCE = instance
+
+                return instance
+            }
+        }
+
+    }
+}
