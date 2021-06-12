7a2c9d14e0ac0a06f9fb21c27b7954d7da69617e
Nelson Jumbo <nelsonjumbo.tw@packlink.com>
2021-06-12 22:44:37 +0200

    adds json annotations to map between snakeCase and camelCase

--- app/src/main/java/com/udacity/asteroidradar/network/DataTransferObjects.kt
+++ app/src/main/java/com/udacity/asteroidradar/network/DataTransferObjects.kt
@@ -1,18 +1,25 @@
 package com.udacity.asteroidradar.network
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import com.squareup.moshi.JsonQualifier
 
 @JsonClass(generateAdapter = true)
 data class NetworkNearEarthObjectsRepresentation(
+    @Json(name="near_earth_objects")
     val nearEarthObjects: Map<String, List<NetworkNearEarthObject>>
 )
 
 @JsonClass(generateAdapter = true)
 data class NetworkNearEarthObject(
     val id: String,
+    @Json(name="absolute_magnitude_h")
     val absoluteMagnitude: Double,
+    @Json(name="estimated_diameter")
     val estimatedDiameter: NetworkEstimatedDiameter,
+    @Json(name="is_potentially_hazardous_asteroid")
     val isPotentiallyHazardousAsteroid: Boolean,
+    @Json(name="close_approach_data")
     val closeApproachData: List<NetworkCloseApproachData>
 )
 
@@ -21,16 +28,21 @@ data class NetworkEstimatedDiameter(
 )
 
 data class Kilometers(
+    @Json(name="estimated_diameter_min")
     val estimatedDiameterMin: String,
+    @Json(name="estimated_diameter_max")
     val estimatedDiameterMax: String
 )
 
 data class NetworkCloseApproachData(
+    @Json(name="relative_velocity")
     val relativeVelocity: NetworkRelatedVelocity,
+    @Json(name="miss_distance")
     val missDistance: NetworkMissDistance
 )
 
 data class NetworkRelatedVelocity(
+    @Json(name = "kilometers_per_second")
     val kilometersPerSecond: String
 )
 
