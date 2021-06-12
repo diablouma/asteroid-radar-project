162b62b3125a6752d732ba5e8fe11565dcd4f649
Nelson Jumbo <nelsonjumbo.tw@packlink.com>
2021-06-12 15:31:50 +0200

    registered nearEarthObjectDao

--- app/src/main/java/com/udacity/asteroidradar/database/AsteroidRadarDatabase.kt
+++ app/src/main/java/com/udacity/asteroidradar/database/AsteroidRadarDatabase.kt
@@ -1,12 +1,22 @@
 package com.udacity.asteroidradar.database
 
 import android.content.Context
+import androidx.lifecycle.LiveData
 import androidx.room.Database
+import androidx.room.Query
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
+interface NearEarthObjectDao {
+    @Query("select * from nearEarthObject")
+    fun getNearEarthObjects(): LiveData<List<NearEarthObject>>
+}
+
+
 @Database(entities = [NearEarthObject::class], version = 1, exportSchema = false)
 abstract class AsteroidRadarDatabase : RoomDatabase() {
+    abstract val nearEarthObjectDao: NearEarthObjectDao
+
     companion object {
         @Volatile
         private var INSTANCE: AsteroidRadarDatabase? = null
