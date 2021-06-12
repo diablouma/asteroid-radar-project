c827602783123f067e9ed7d0080d0448a95e81d2
Nelson Jumbo <nelsonjumbo.tw@packlink.com>
2021-06-13 00:08:10 +0200

    use repo in mainviewmodel to refresh cache

--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,37 +1,29 @@
 package com.udacity.asteroidradar.main
 
+import android.app.Application
 import android.util.Log
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.udacity.asteroidradar.BuildConfig
+import com.udacity.asteroidradar.database.AsteroidRadarDatabase
 import com.udacity.asteroidradar.dateUtils.toStringRepresentation
 import com.udacity.asteroidradar.network.AsteroidRadarApi
 import com.udacity.asteroidradar.network.asDatabaseModel
 import com.udacity.asteroidradar.network.asDomainModel
+import com.udacity.asteroidradar.repository.NearEarthObjectRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 
-class MainViewModel : ViewModel() {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+    private val database = AsteroidRadarDatabase.getInstance(application)
+    private val nearEarthObjectRepository = NearEarthObjectRepository(database)
+
     init {
         viewModelScope.launch {
-            withContext(Dispatchers.IO) {
-                val nearEarthObjects = AsteroidRadarApi.retrofitService.getNearEarthObjects(
-                    LocalDate.of(2021, 6, 12).format(DateTimeFormatter.ofPattern(
-                        API_QUERY_DATE_FORMAT
-                    )),
-                    LocalDate.of(2021, 6, 13).format(DateTimeFormatter.ofPattern(
-                        API_QUERY_DATE_FORMAT
-                    )),
-                    BuildConfig.NASA_API_KEY
-                ).await()
-                Log.i("Mainactivity", nearEarthObjects.toString())
-                Log.i("Mainactivity", nearEarthObjects.networkNearEarthObjectsContainer.keys.size.toString())
-                Log.i("Mainactivity", nearEarthObjects.asDomainModel().size.toString())
-                Log.i("Mainactivity", nearEarthObjects.asDatabaseModel().size.toString())
-            }
+            nearEarthObjectRepository.refreshNearEarthObjects()
         }
     }
 }
