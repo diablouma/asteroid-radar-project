8bfe6bc890f61fd7ce487b91613d121bf5190403
Nelson Jumbo <nelsonjumbo.tw@packlink.com>
2021-06-13 09:41:07 +0200

    creates worker that is intended to refresh the db data

---
+++ app/src/main/java/com/udacity/asteroidradar/work/RefreshDataWorker.kt
@@ -0,0 +1,24 @@
+package com.udacity.asteroidradar.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.udacity.asteroidradar.database.AsteroidRadarDatabase
+import com.udacity.asteroidradar.repository.NearEarthObjectRepository
+import retrofit2.HttpException
+
+class RefreshDataWorker(appContext: Context, params: WorkerParameters) :
+    CoroutineWorker(appContext, params) {
+    override suspend fun doWork(): Result {
+        val database = AsteroidRadarDatabase.getInstance(applicationContext)
+        val nearEarthObjectRepository = NearEarthObjectRepository(database)
+        nearEarthObjectRepository.refreshNearEarthObjects()
+
+        return try {
+            Result.success()
+        } catch (exception: HttpException) {
+            Result.retry()
+        }
+    }
+
+}
